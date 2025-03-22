5eb6be29604c918d7432a8f7d0e3736a81842d4b
Arnold Wainaina <arnoldwainaina2865@gmail.com>
2025-03-23 00:51:32 +0300

    lab12 assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const workerProfile = {
+    name: "Alex Doe",
+    streetAddress: "123 Elm Street"
+  };
+  
+  const employee = workerProfile; 
+  
+  function updateEmployeeWithKeyAndValue(emp, key, value) {
+    return { ...emp, [key]: value };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(emp, key, value) {
+    emp[key] = value;
+    return emp;
+  }
+  
+  function deleteFromEmployeeByKey(emp, key) {
+    const freshCopy = { ...emp };
+    delete freshCopy[key];
+    return freshCopy;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(emp, key) {
+    delete emp[key];
+    return emp;
+  }
+  
